aea61c543f0d57b2a5677435bc9be231bd10d937
Derrell Downey Jr <derrell.downey@gmail.com>
2024-08-26 19:05:47 -0400

    made theme transparent

--- lua/ddowneyjr/plugins/colorscheme.lua
+++ lua/ddowneyjr/plugins/colorscheme.lua
@@ -44,8 +44,11 @@ return {
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
 		config = function()
-			vim.cmd([[colorscheme gruvbox]])
+			-- Following code makes theme transparent
+			vim.cmd("let g:gruvbox_transparent_bg = 1")
+			vim.cmd("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+			vim.cmd("colorscheme gruvbox")
 		end,
-		opts = ...,
+		opts = {},
 	},
 }
