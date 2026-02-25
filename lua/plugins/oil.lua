4950d99addd97c6d06ed5994de691818809c4cbd
Derrell Downey Jr <ddowneyjr@mbp.local>
2026-02-25 00:28:13 -0500

    added border to oil float

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,12 @@ return {
     config = function()
       vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#ebdbb2', bg = 'none' })
       local oil = require("oil")
-      oil.setup()
+      oil.setup({
+        float = {
+          padding = 2,
+          border = "rounded",
+        },
+      })
       vim.keymap.set("n", "-", oil.toggle_float, {})
     end,
   },
